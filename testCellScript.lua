e83899e4e4a364acc7bb5da5869b154b0d795d8c
KINGTUT10101 <kingtut10101@gmail.com>
2025-02-23 11:33:20 +0000

    Remake test cell script to adhere to the upcoming recursive action list system

--- testCellScript.lua
+++ testCellScript.lua
@@ -1,25 +1,54 @@
 -- An example of a scriptList attribute of a cell object
 local exScriptList = {
     {
-        id = "readInput",
-        interDict = {
-            saveTo = "var1"
-        },
-    },
-    {
-        id = "addVariables",
-        interDict = {
-            saveTo = "var0_1",
-            term1 = "var0_1",
-            term2 = "var1_4"
-        },
-    },
-    {
-        id = "ifStruct",
-        interDict = {
-            compTo = "var1"
-        },
-    },
+        id = "startScript",
+        vars = 2,
+        scope = {
+            {
+                id = "readInput",
+                args = {
+                    saveTo = "var1_1",
+                },
+            },
+            {
+                id = "addVariables",
+                args = {
+                    saveTo = "var1_1",
+                    term1 = "var1_1",
+                    term2 = "var1_2",
+                },
+            },
+            {
+                id = "ifStruct",
+                args = {
+                    term1 = "var1_1",
+                    term2 = "var1_1",
+                    op = "~=",
+                },
+                vars = 2,
+                scope = {
+                    {
+                        id = "applyDamage",
+                        args = {},
+                    },
+                    {
+                        id = "isTaken",
+                        args = {
+                            assignTo = "var2_1"
+                        },
+                    },
+                    {
+                        id = "addVariables",
+                        args = {
+                            assignTo = "var1_2",
+                            term1 = "var2_2",
+                            term2 = "var2_1",
+                        },
+                    },
+                },
+            }
+        }
+    }
 }
 
 return exScriptList
